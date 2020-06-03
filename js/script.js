1138c289ee8a794444b62a6e236e7d94e0e4226e
Rajdeep Saha <rsaha734@gmail.com>
2020-06-03 22:38:54 +0530

    Portfolio

--- js/script.js
+++ js/script.js
@@ -14,8 +14,11 @@ $(document).ready(function() {
     });
 
     $('.owl-carousel').owlCarousel({
-        loop:false,
+        loop:true,
         items: 4,
+        autoplay:true,
+        autoplayTimeout:4000,
+        autoplayHoverPause:true,
         responsive:{
             0:{
                 items:1
@@ -59,6 +62,32 @@ $(document).ready(function() {
             });
             countupfinished=true;
         }
+    });
+    $("[data-fancybox]").fancybox();
+
+    $(".items").isotope({
+        filter: '*',
+        animationOptions: {
+            duration: 1500,
+            easing: 'linear',
+            queue: false
+        }
+    });
+    $("#filters a").click(function(){
+        $("#filters .current").removeClass("current");
+        $(this).addClass("current");
+        var selector=$(this).attr("data-filter");
+
+        $(".items").isotope({
+                filter: selector,
+                animationOptions: {
+                    duration: 1500,
+                    easing: 'linear',
+                    queue: false
+            }
+        });
+
+        return false;
     });
     
 });
