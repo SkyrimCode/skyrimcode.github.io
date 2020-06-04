1319c8f68cf250d46aa873b09c2c641bf4395abe
Rajdeep Saha <rsaha734@gmail.com>
2020-06-04 21:25:59 +0530

    Copyrights & Minor mods

--- js/script.js
+++ js/script.js
@@ -89,5 +89,28 @@ $(document).ready(function() {
 
         return false;
     });
+
+    $("#navigation li a").click(function(e){
+        e.preventDefault();
+        var target= $(this).attr("href");
+        var targetpos= $(target).offset().top;
+        $("html,body").animate({ scrollTop: targetpos - 50 }, "slow");
+    });
+
+    const nav = $("#navigation");
+    const navTop= nav.offset().top;
+    $(window).on("scroll",stickyNavigation);
+
+    function stickyNavigation(){
+        var body= $("body");
+        if($(window).scrollTop()>=navTop){
+            body.css("padding-top",nav.outerHeight() + "px");
+            body.addClass("fixedNav");
+        }
+        else{
+            body.css("padding-top",0);
+            body.removeClass("fixedNav");
+        }
+    }
     
 });
